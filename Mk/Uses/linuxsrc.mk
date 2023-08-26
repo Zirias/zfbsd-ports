d08cac2a4ee21134d29933eb7d4d597c5423dbbd
Felix Palmen <zirias@FreeBSD.org>
2023-08-26 09:40:27 +0000

    linuxsrc-libarchive: Add new port
    Add libarchive for Linux.

--- Mk/Uses/linuxsrc.mk
+++ Mk/Uses/linuxsrc.mk
@@ -88,6 +88,7 @@ LINUXSRC_V_MPFR=	4.2.0
 
 # optional components
 _linuxsrc_expat=	textproc:expat
+_linuxsrc_libarchive=	archivers:libarchive
 
 # standard Linux variables
 LINUXSRC_ARCH=		${ARCH:S/amd64/x86_64/}
