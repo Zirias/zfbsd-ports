bee5bc667b60607112df0eff04decd5f44e2f3f2
Felix Palmen <zirias@FreeBSD.org>
2023-08-30 17:04:23 +0000

    x11-fonts/linuxsrc-fontconfig: Add new port
    Add fontconfig for Linux.

--- Mk/Uses/linuxsrc.mk
+++ Mk/Uses/linuxsrc.mk
@@ -94,6 +94,7 @@ _linuxsrc_brotli=	archivers:brotli
 _linuxsrc_cmake=	devel:cmake
 _linuxsrc_elfutils=	devel:elfutils
 _linuxsrc_expat=	textproc:expat
+_linuxsrc_fontconfig=	x11-fonts:fontconfig
 _linuxsrc_freetype=	print:freetype
 _linuxsrc_gettext=	devel:gettext
 _linuxsrc_glib=		devel:glib
