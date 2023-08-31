6ee47acf33d47a53a36c963112bf96ff6aeb822c
Felix Palmen <zirias@FreeBSD.org>
2023-08-31 14:18:36 +0000

    converters/linuxsrc-fribidi: Add new port
    Add fribidi for Linux.

--- Mk/Uses/linuxsrc.mk
+++ Mk/Uses/linuxsrc.mk
@@ -97,6 +97,7 @@ _linuxsrc_elfutils=	devel:elfutils
 _linuxsrc_expat=	textproc:expat
 _linuxsrc_fontconfig=	x11-fonts:fontconfig
 _linuxsrc_freetype=	print:freetype
+_linuxsrc_fribidi=	converters:fribidi
 _linuxsrc_gettext=	devel:gettext
 _linuxsrc_gintrospect=	devel:gobject-introspection
 _linuxsrc_glib=		devel:glib
