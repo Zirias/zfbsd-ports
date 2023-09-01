ec6f9b8a3a005c0122b7c8c6fca8318a9b91577a
Felix Palmen <zirias@FreeBSD.org>
2023-09-01 08:57:14 +0000

    multimedia/linuxsrc-libva: Add new port
    Add libva for Linux.

--- Mk/Uses/linuxsrc.mk
+++ Mk/Uses/linuxsrc.mk
@@ -127,6 +127,7 @@ _linuxsrc_libpng=	graphics:libpng
 _linuxsrc_libsm=	x11:libSM
 _linuxsrc_libtasn1=	security:libtasn1
 _linuxsrc_libtool=	devel:libtool
+_linuxsrc_libva=	multimedia:libva
 _linuxsrc_libunibreak=	textproc:libunibreak
 _linuxsrc_libuuid=	devel:libuuid
 _linuxsrc_libuv=	devel:libuv
